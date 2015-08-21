aeb830bc7cfcf7e15b6b5eb4574dfb67ead484fa
M D <dudecrushman@gmail.com>
2015-08-20 23:04:41 -0700

    Cleanup fix.

--- screeps/main.js
+++ screeps/main.js
@@ -4,11 +4,11 @@ if(Object.keys(Game.creeps).length < 10) {
     Game.spawns['spawn1'].createCreep([WORK, CARRY, MOVE], undefined, {'role': 'harvester'});
 }
 
-var x = "";
 for(var i in Memory.creeps) {
-    x = " " + x + i + " " + Game.creeps[i];
+    if(Memory.creeps[i].state && !Game.creeps[i]) {
+        delete Memory.creeps[i];
+    }
 }
-console.log(x);
 
 for (var name in Game.creeps) {
     var creep = Game.creeps[name];

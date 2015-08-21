4d0b63eaec0ba4535bf12619ae8b205837aaa72b
M D <dudecrushman@gmail.com>
2015-08-20 21:25:59 -0700

    Logging change.

--- screeps/main.js
+++ screeps/main.js
@@ -1,8 +1,7 @@
 var harvester = require('harvester');
 
 if(Game.creeps.length < 10) {
-    console.log('creating creep');
-    Game.spawns['spawn1'].createCreep([WORK, CARRY, MOVE], undefined, {'role': 'harvester'});
+    console.log('creating creep' + Game.spawns['spawn1'].createCreep([WORK, CARRY, MOVE], undefined, {'role': 'harvester'}));
 }
 
 for (var name in Game.creeps) {

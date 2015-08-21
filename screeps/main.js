b6f2d55c8fc699ab5b28b66d3f9cc77144eb9ca6
M D <dudecrushman@gmail.com>
2015-08-20 21:24:51 -0700

    Spawn access type change.

--- screeps/main.js
+++ screeps/main.js
@@ -2,11 +2,11 @@ var harvester = require('harvester');
 
 if(Game.creeps.length < 10) {
     console.log('creating creep');
-    Game.spawns.spawn1.createCreep([WORK, CARRY, MOVE], undefined, {'role': 'harvester'});
+    Game.spawns['spawn1'].createCreep([WORK, CARRY, MOVE], undefined, {'role': 'harvester'});
 }
 
 for (var name in Game.creeps) {
     var creep = Game.creeps[name];
-    console.log('harvester cakking');
+    console.log('harvester calling');
     harvester(creep);
 }
